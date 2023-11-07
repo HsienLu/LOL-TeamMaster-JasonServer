188395344770acda23e17664d254cb632d80a1bf
HsienLu <sjps946403@gmail.com>
2023-11-07 21:05:17 +0800

    新增篩選隊友的路由功能

--- server.js
+++ server.js
@@ -1,33 +1,48 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('db.json'); // 假設您的JSON數據文件名為db.json
-// const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const data = require('./db.json');
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+server.use(middlewares)
+server.get('/echo', (req, res) => {
+    res.jsonp(req.query)
+  })
+  server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
 
-// // 自定義中間件來處理關聯資訊
-// server.use((req, res, next) => {
-//   if (req.method === 'GET' && req.url.includes('/teams/')) {
-//     // 處理需要關聯使用者詳細資訊的路由，例如 /teams/1
-//     const teamId = parseInt(req.params.id);
-//     const teams = router.db.get('teams').value();
-//     const users = router.db.get('users').value();
+server.get('/teamsMember/:id', (req, res) => {
+    
+    const teamId = req.params.id-1;
+    let teamMembers =[]
+    if (data.teams[teamId]) {
+        const teamMerberId=data.teams[teamId].teamMerberId
+        // console.log(teamMerberId)
+        
+        teamMerberId.forEach((v)=>{
+            // console.log(v)
+            const teamMember =data.users.find((i)=>i.id === v)
+            teamMembers.push(teamMember)
+            // let AAA=data.users.filter((i)=>{i.id===v})
+            
+            // console.log(AAA)
+        })
+        // const teamMemberIds = data.teams;
+        console.log(teamMembers)
+        res.status(200).json(teamMembers);
+        
+    } else {
+        res.status(404).json({ message: '找不到隊伍' });
+    }
 
-//     const team = teams.find((t) => t.id === teamId);
-//     if (team) {
-//       const usersInTeam = team.userIds.map((userId) => users.find((user) => user.id === userId));
-//       team.user = usersInTeam;
-//     }
-
-//     res.json(team);
-//   } else {
-//     // 對於其他路由，使用JSON Server的默認中間件
-//     next();
-//   }
-// });
-
-// server.use(middlewares);
-// server.use(router);
-
-// const port = 3000;
-// server.listen(port, () => {
-//   console.log(`JSON Server運行在 http://localhost:${port}`);
-// });
+  });
+  
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+  })
